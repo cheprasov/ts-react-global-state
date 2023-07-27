6006c8244113a80f3f42050497a7db70c64fee47
Alexander Cheprasov <acheprasov84@gmail.com>
2023-07-27 23:33:39 +0100

    wip

--- dist/src/index.d.ts
+++ dist/src/index.d.ts
@@ -1,4 +1,4 @@
-export { createGlobalState, useGlobalState, createGlobalReducer, useGlobalReducer, createGlobalScope, createMultiGlobalScopes, useGlobalScope, withGlobalScope, } from './GlobalState/GlobalState';
+export { useGlobalReducer, createGlobalStates, useGlobalScope, withGlobalScope, } from './GlobalState/GlobalState';
 export { Scope } from './GlobalState/Scope';
 export { GlobalScope, hydrateGlobalScope } from './GlobalState/createGlobalScope';
 export { GlobalReducer } from './GlobalState/GlobalReducer';
