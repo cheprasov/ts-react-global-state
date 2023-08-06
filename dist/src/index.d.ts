9a57b488d75b3ebea7a10f88653c774091fd8cae
Alexander Cheprasov <acheprasov84@gmail.com>
2023-08-06 15:41:54 +0100

    upd

--- dist/src/index.d.ts
+++ dist/src/index.d.ts
@@ -1,4 +1,5 @@
 export { useGlobalScope } from './global-scope/useGlobalScope';
 export { createGlobalScope } from './global-scope/createGlobalScope';
+export { withGlobalScope } from './global-scope/withGlobalScope';
 export { Scope } from './global-scope/Scope';
 export type { TScope } from './global-scope/types';
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export { useGlobalScope } from './global-scope/useGlobalScope';
 export { createGlobalScope } from './global-scope/createGlobalScope';
+export { withGlobalScope } from './global-scope/withGlobalScope';
 export { Scope } from './global-scope/Scope';
 
 export type { TScope } from './global-scope/types';
